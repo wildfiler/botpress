0476e1090e41bc1f63f1d8ee5d699e5b1f5c3f83
Ivan Arakcheev <wildfiler0@gmail.com>
2016-09-23 14:47:08 +0300

    Serialize raw field with JSON

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,22 @@
 class Item < ApplicationRecord
+  IMAGE_URL = 'https://steamcommunity-a.akamaihd.net/economy/image/'.freeze
+
   belongs_to :game
   belongs_to :bot
 
   validates :bot_id, :game_id, presence: true
+
+  serialize :raw, JSON
+
+  def icon_url
+    "#{IMAGE_URL}#{raw['icon_url']}"
+  end
+
+  def icon_large_url
+    "#{IMAGE_URL}#{raw['icon_url_large']}"
+  end
+
+  def name_color
+    raw['name_color']
+  end
 end
