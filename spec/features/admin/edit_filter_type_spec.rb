e47e70dae18455a8e0c89b9052146071493539d7
Paul Pavlovsky <fatpaher@gmail.com>
2016-11-04 15:01:40 +0200

    admin/filter_type#edit spec

---
+++ spec/features/admin/edit_filter_type_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'admin visit admin/filter_type#edit page' do
+  it 'can create new filter type' do
+    old_game = create(:game)
+    new_game = create(:game)
+    filter_type = create(:filter_type, game: old_game)
+    new_filter_type_name = 'very new filter_type name'
+
+    visit edit_admin_filter_type_path(filter_type, as: create(:user, :admin))
+    fill_in('Name', with: new_filter_type_name)
+    select(new_game)
+    click_on 'Edit Filter type'
+
+    expect(page).to have_css('.filter_type_name', text: new_filter_type_name)
+    expect(page).to have_content(new_game)
+  end
+end
