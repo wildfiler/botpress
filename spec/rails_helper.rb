e6e04b758e2c5ec1741da16c0c224a318cf07c5c
Paul Pavlovsky <fatpaher@gmail.com>
2016-11-11 16:51:42 +0200

    remove javascript_driver config from Rspec.config block

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,5 +48,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  Capybara.javascript_driver = :webkit
 end
+
+Capybara.javascript_driver = :webkit
