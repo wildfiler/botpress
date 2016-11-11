d94a9a61cca5cf5936fd887ac90a8ca2c157e191
Paul Pavlovsky <fatpaher@gmail.com>
2016-11-11 16:41:54 +0200

    Remove unneeded specs from filter_type model

--- spec/models/filter_type_spec.rb
+++ spec/models/filter_type_spec.rb
@@ -17,19 +17,5 @@ describe FilterType do
       is_expected.to accept_nested_attributes_for(:filter_values).
         allow_destroy(true)
     end
-
-    it 'creates filter_values' do
-      expect do
-        create(:filter_type,
-               filter_values_attributes: [attributes_for(:filter_value)])
-      end.to change(FilterValue, :count).by(1)
-    end
-
-    it "won't create filter_value if field is blank" do
-      expect do
-        create(:filter_type,
-               filter_values_attributes: [attributes_for(:filter_value, value: '')])
-      end.not_to change(FilterValue, :count)
-    end
   end
 end
