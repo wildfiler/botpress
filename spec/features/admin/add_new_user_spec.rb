88476b91cdabc9e005bb855df66f0e8ca8cbb2b7
Paul Pavlovsky <fatpaher@gmail.com>
2016-11-29 16:52:40 +0200

    rework admin create new user with password confirmation

--- spec/features/admin/add_new_user_spec.rb
+++ spec/features/admin/add_new_user_spec.rb
@@ -9,9 +9,26 @@ describe 'admin visit admin new user page' do
     visit new_admin_user_path(as: admin)
     fill_in('Email', with: user_email)
     fill_in('Password', with: user_password)
+    fill_in 'Password confirmation', with: user_password
     check('Promote to admin')
     click_on('Create User')
 
     expect(page).to have_content(user_email)
+  end
+
+  it "can't create user with wrong password confirmation" do
+    admin = create(:user, :admin)
+    user_email = 'example@foo.bar'
+    user_password = '12345678'
+    wrong_password = '87654321'
+
+    visit new_admin_user_path(as: admin)
+    fill_in('Email', with: user_email)
+    fill_in('Password', with: user_password)
+    fill_in 'Password confirmation', with: wrong_password
+    check('Promote to admin')
+    click_on('Create User')
+
+    expect(page).not_to have_content(user_email)
   end
 end
